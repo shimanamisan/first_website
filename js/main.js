20e6a5d2e7c37b97e1145ef166b01517868d514a
ehimedamikan <mikan.sup.all@gmail.com>
2020-04-04 09:21:21 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+    
+$(function(){
+    $('.js-menu-trigger').on('click',function(){
+        $(this).toggleClass('active');
+        $('.sp-menu').toggleClass('sp-menu-Active');
+    });
+
+    
+});
+
