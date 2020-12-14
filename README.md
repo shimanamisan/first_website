68eddac0b4d13f7cab9cd3e696fda7c6c66e8402
shimanamisan <mikan.sup.all@gmail.com>
2020-12-14 12:23:27 +0900

    [update] READMEを編集

--- README.md
+++ README.md
@@ -8,14 +8,101 @@
 
 プログラミング学習を開始して 1 ヶ月ほど経過した後に、学習したことのアウトプットとして Web サイト作成しました。
 
+## 使用技術
+
+- HTML
+- CSS
+- jQuery（Ver 3.4.1）
+
 ## 工夫した点
 
 - レスポンシブデザイン（スマホのみ）
 - メニューリストの画像スライダーはプラグインを使用せず、jQurty を使用し自分で作成
 - 画像スライダーは**this の向き先を意識し、クロージャーを使用**して作成
 
-## 使用技術
+![shimanami_cafe](https://user-images.githubusercontent.com/49751604/102036772-0dabfb00-3e07-11eb-9529-39d85b732972.gif)
 
-- HTML
-- CSS
-- jQuery（Ver 3.4.1）
+```js
+// コード抜粋
+
+let slider = (function () {
+	// モーダルが display:none; で要素の幅取得できないので、即時関数内で一旦要素を表示させる
+	let $modalMain = document.querySelector(".modal-main");
+	// display:block; とする
+	$modalMain.style.display = "block";
+
+	// 各種スライダーに必要な要素を取得
+	let $sliderContainer = $(".slider_container");
+	let $sliderItem = $(".slider_item");
+
+	// スライドさせる要素の個数を取得
+	let sliderItemNum = $(".slider_item").length;
+
+	// スライドさせる要素の幅を取得
+	let sliderItemWidth = $(".slider_item").innerWidth();
+
+	// スライドさせる要素の幅を個数から、トータルで移動させる幅を算出
+	let sliderContainerWidth = sliderItemWidth * sliderItemNum;
+
+	let currentItemNum = 1;
+
+	let DURATION = 500;
+
+	// 要素の幅を取得したので、ここで非表示にしている
+	$modalMain.style.display = "none";
+
+	return {
+		// オブジェクトを返す
+		sliderNext: function () {
+			if (currentItemNum < sliderItemNum) {
+				$sliderContainer.animate(
+					{
+						left: "-=" + sliderItemWidth + "px",
+					},
+					DURATION
+				);
+				currentItemNum++;
+				console.log(currentItemNum);
+			}
+		},
+		slidePrev: function () {
+			if (currentItemNum > 1) {
+				$sliderContainer.animate(
+					{
+						left: "+=" + sliderItemWidth + "px",
+					},
+					DURATION
+				);
+				currentItemNum--;
+			}
+		},
+		setModalNum: function (num) {
+			currentItemNum = num;
+		},
+		clearModalNum: function () {
+			currentItemNum = 0;
+		},
+		init: function () {
+			// 計算したコンテナのコンテナアイテムの全幅をスタイル属性に埋め込む
+			$sliderContainer.attr("style", "width: " + sliderContainerWidth + "px");
+			// スライドさせる要素の幅をスタイル属性に埋め込む（レスポンシブ対応にするため）
+			$sliderItem.attr("style", "width:" + sliderItemWidth + "px");
+			// ここでthisとすることで、thisのスコープがオブジェクト自体になる。変数に格納することでthisを保持できる
+			let that = this;
+
+			$(".js-slide-next").on("click", function () {
+				// thatがオブジェクトを指しているので、関数オブジェクトを呼ぶことができる
+				that.sliderNext();
+			});
+			$(".js-slide-prev").on("click", function () {
+				that.slidePrev();
+			});
+
+			// console.log("slider.init実行時 " + currentItemNum);
+		},
+	};
+})();
+
+// オブジェクト内の初期化用メソッドを呼び出す
+slider.init();
+```
--- js/main.js
+++ js/main.js
@@ -1,168 +1,159 @@
 $(function () {
-  // ハンバーガーメニュー表示
-  $(".js-menu-trigger").on("click", function () {
-    $(this).toggleClass("active");
-    $(".sp-menu").toggleClass("sp-menu-Active");
-  });
+	// ハンバーガーメニュー表示
+	$(".js-menu-trigger").on("click", function () {
+		$(this).toggleClass("active");
+		$(".sp-menu").toggleClass("sp-menu-Active");
+	});
 
-  /****************************************
+	/****************************************
   モーダルウィンドウ表示処理
   *****************************************/
-  // 画像を配列に格納
-  let images = [
-    "images/menuList/menu1.jpg",
-    "images/menuList/menu2.jpg",
-    "images/menuList/menu3.jpg",
-    "images/menuList/menu4.jpg",
-    "images/menuList/menu5.jpg",
-    "images/menuList/menu6.jpg",
-    "images/menuList/menu7.jpg",
-    "images/menuList/menu8.jpg",
-    "images/menuList/menu9.jpg",
-  ];
-
-  images.forEach((element, index) => {
-    let images_id = index;
-    let i = ++index;
-    // img要素を作成
-    let newImg = $("<img>", {
-      src: images[images_id],
-      class: `slider_item slider_item${i}`,
-      "data-slider": i,
-      alt: "テスト",
-    });
-    // 子要素として作成
-    $(".slider_container").append(newImg);
-  });
-  /****************************************
+	// 画像のファイルパスを配列に格納
+	let images = [
+		"images/menuList/menu1.jpg",
+		"images/menuList/menu2.jpg",
+		"images/menuList/menu3.jpg",
+		"images/menuList/menu4.jpg",
+		"images/menuList/menu5.jpg",
+		"images/menuList/menu6.jpg",
+		"images/menuList/menu7.jpg",
+		"images/menuList/menu8.jpg",
+		"images/menuList/menu9.jpg",
+	];
+	images.forEach((element, index) => {
+		let images_id = index;
+		// 配列のインデックスに1を足して要素に番号を付与する
+		let i = ++index;
+		// img要素を作成
+		let newImg = $("<img>", {
+			src: images[images_id],
+			class: `slider_item slider_item${i}`,
+			"data-slider": i,
+			alt: "メニュー画像",
+		});
+		// 子要素として作成
+		$(".slider_container").append(newImg);
+	});
+	/****************************************
   モーダル表示関係の処理
   *****************************************/
-  let currentFlg = 0; // モーダルを閉じる際に処理が終わる前に連続クリック出来ないようにするためのフラグ
-  let $modalOpen = $(".js-modal-open");
-  let $sliderItem = $(".slider_item");
-  $modalOpen.on("click", function (e) {
-    if (currentFlg === 0) {
-      let sliderItemWidth = $(".slider_item").innerWidth();
-      // 画像のdata属性を取得
-      let currentModalItem = $(this).children("img").data("id");
-      console.log(currentModalItem);
-      // 画像スライダーのクロージャーで管理している変数に、クリックした画像のdata属性の値をセット
-      slider.setModalNum(currentModalItem);
-
-      $sliderItem.each((index, element) => {
-        if (currentModalItem === $(element).data("slider")) {
-          // console.log("クリックしたモーダルの現在値 : " + currentModalItem);
-          $(".slider_container").css({
-            left: "-=" + sliderItemWidth * index + "px",
-          });
-          // console.log(sliderItemWidth * --currentModalItem);
-          return false;
-        }
-      });
-      $(".modal-main").fadeIn();
-    }
-  });
-  /****************************************
+	let currentFlg = 0; // モーダルを閉じる際に処理が終わる前に連続クリック出来ないようにするためのフラグ
+	let $modalOpen = $(".js-modal-open");
+	let $sliderItem = $(".slider_item");
+	$modalOpen.on("click", function (e) {
+		if (currentFlg === 0) {
+			let sliderItemWidth = $(".slider_item").innerWidth();
+			// 画像のdata属性を取得
+			let currentModalItem = $(this).children("img").data("id");
+			// 画像スライダーのクロージャーで管理している変数に、クリックした画像のdata属性の値をセット
+			slider.setModalNum(currentModalItem);
+			$sliderItem.each((index, element) => {
+				if (currentModalItem === $(element).data("slider")) {
+					$(".slider_container").css({
+						left: "-=" + sliderItemWidth * index + "px",
+					});
+					return false;
+				}
+			});
+			$(".modal-main").fadeIn();
+		}
+	});
+	/****************************************
   モーダルクローズ時の処理
   *****************************************/
-  $(".js-modal-close").on("click", function () {
-    if (currentFlg === 0) {
-      currentFlg = 1;
-      slider.clearModalNum(); // モーダルの画像スライダー現在値をクリア
-      $(".modal-main").fadeOut();
-      setTimeout(() => {
-        currentFlg = 0;
-        $(".slider_container").css({
-          left: 0 + "px",
-        });
-      }, 400);
-    }
-  });
+	$(".js-modal-close").on("click", function () {
+		if (currentFlg === 0) {
+			currentFlg = 1;
+			slider.clearModalNum(); // モーダルの画像スライダー現在値をクリア
+			$(".modal-main").fadeOut();
+			// モーダルが閉じた後に、画像スライダーの現在の移動距離（位置）を初期化
+			setTimeout(() => {
+				currentFlg = 0;
+				$(".slider_container").css({
+					left: 0 + "px",
+				});
+			}, 400);
+		}
+	});
 
-  let slider = (function () {
-    // モーダルが display:none; で要素の幅取得できないので、即時関数内で一旦要素を表示させる
-    let $modalMain = document.querySelector(".modal-main");
-    $modalMain.style.display = "block";
+	let slider = (function () {
+		// モーダルが display:none; で要素の幅取得できないので、即時関数内で一旦要素を表示させる
+		let $modalMain = document.querySelector(".modal-main");
+		// display:block; とする
+		$modalMain.style.display = "block";
 
-    let $sliderContainer = $(".slider_container");
-    let $sliderItem = $(".slider_item");
+		// 各種スライダーに必要な要素を取得
+		let $sliderContainer = $(".slider_container");
+		let $sliderItem = $(".slider_item");
 
-    // スライドさせる要素の個数を取得
-    let sliderItemNum = $(".slider_item").length;
-    // console.log("sliderItemNum: " + sliderItemNum);
+		// スライドさせる要素の個数を取得
+		let sliderItemNum = $(".slider_item").length;
 
-    // スライドさせる要素の幅を取得
-    let sliderItemWidth = $(".slider_item").innerWidth();
-    // console.log("sliderItemWidth: " + sliderItemWidth);
+		// スライドさせる要素の幅を取得
+		let sliderItemWidth = $(".slider_item").innerWidth();
 
-    // スライドさせる要素の幅を個数から、トータルで移動させる幅を算出
-    let sliderContainerWidth = sliderItemWidth * sliderItemNum;
-    // console.log("slderContainer: " + sliderContainerWidth);
+		// スライドさせる要素の幅を個数から、トータルで移動させる幅を算出
+		let sliderContainerWidth = sliderItemWidth * sliderItemNum;
 
-    let currentItemNum = 1;
+		let currentItemNum = 1;
 
-    let DURATION = 500;
+		let DURATION = 500;
 
-    // 要素の幅を取得したので、ここで非表示にしている
-    $modalMain.style.display = "none";
+		// 要素の幅を取得したので、ここで非表示にしている
+		$modalMain.style.display = "none";
 
-    return {
-      // オブジェクトを返す
-      sliderNext: function () {
-        if (currentItemNum < sliderItemNum) {
-          $sliderContainer.animate(
-            {
-              left: "-=" + sliderItemWidth + "px",
-            },
-            DURATION
-          );
-          currentItemNum++;
-          console.log(currentItemNum);
-        }
-      },
-      slidePrev: function () {
-        if (currentItemNum > 1) {
-          $sliderContainer.animate(
-            {
-              left: "+=" + sliderItemWidth + "px",
-            },
-            DURATION
-          );
-          currentItemNum--;
-        }
-      },
-      setModalNum: function (num) {
-        // console.log("currentItemNumを呼び出してセットする（セット前）" + currentItemNum);
+		return {
+			// オブジェクトを返す
+			sliderNext: function () {
+				if (currentItemNum < sliderItemNum) {
+					$sliderContainer.animate(
+						{
+							left: "-=" + sliderItemWidth + "px",
+						},
+						DURATION
+					);
+					currentItemNum++;
+					console.log(currentItemNum);
+				}
+			},
+			slidePrev: function () {
+				if (currentItemNum > 1) {
+					$sliderContainer.animate(
+						{
+							left: "+=" + sliderItemWidth + "px",
+						},
+						DURATION
+					);
+					currentItemNum--;
+				}
+			},
+			setModalNum: function (num) {
+				currentItemNum = num;
+			},
+			clearModalNum: function () {
+				currentItemNum = 0;
+			},
+			init: function () {
+				// 計算したコンテナのコンテナアイテムの全幅をスタイル属性に埋め込む
+				$sliderContainer.attr("style", "width: " + sliderContainerWidth + "px");
+				// スライドさせる要素の幅をスタイル属性に埋め込む（レスポンシブ対応にするため）
+				$sliderItem.attr("style", "width:" + sliderItemWidth + "px");
+				// ここでthisとすることで、thisのスコープがオブジェクト自体になる。変数に格納することでthisを保持できる
+				let that = this;
 
-        currentItemNum = num;
+				$(".js-slide-next").on("click", function () {
+					// thatがオブジェクトを指しているので、関数オブジェクトを呼ぶことができる
+					that.sliderNext();
+				});
+				$(".js-slide-prev").on("click", function () {
+					that.slidePrev();
+				});
 
-        // console.log("currentItemNumを呼び出してセットする（セット後）" + currentItemNum);
-      },
-      clearModalNum: function () {
-        currentItemNum = 0;
+				// console.log("slider.init実行時 " + currentItemNum);
+			},
+		};
+	})();
 
-        // console.log("currentItemNumリセット後" + currentItemNum);
-      },
-      init: function () {
-        // 計算したコンテナのコンテナアイテムの全幅をスタイル属性に埋め込む
-        $sliderContainer.attr("style", "width: " + sliderContainerWidth + "px");
-        // スライドさせる要素の幅をスタイル属性に埋め込む（レスポンシブ対応にするため）
-        $sliderItem.attr("style", "width:" + sliderItemWidth + "px");
-        // ここでthisとすることで、thisのスコープがオブジェクト自体になる。変数に格納することでthisを保持できる
-        let that = this;
-
-        $(".js-slide-next").on("click", function () {
-          // thatがオブジェクトを指しているので、関数オブジェクトを呼ぶことができる
-          that.sliderNext();
-        });
-        $(".js-slide-prev").on("click", function () {
-          that.slidePrev();
-        });
-
-        // console.log("slider.init実行時 " + currentItemNum);
-      },
-    };
-  })();
-
-  slider.init();
+	// オブジェクト内の初期化用メソッドを呼び出す
+	slider.init();
 });
